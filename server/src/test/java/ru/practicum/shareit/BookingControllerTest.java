55d8dad2f0a1dbb11a8f23291a3bdbd2803c7dce
Anton Drozdov <anton.drozdov2015@yandex.ru>
2024-11-12 05:22:49 +0300

    init commit. Modified according tech tasks

--- server/src/test/java/ru/practicum/shareit/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/BookingControllerTest.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
